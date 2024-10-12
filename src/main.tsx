ce9391421d17e7f8858bf45589e0d1afbb99d7a2
rmedcraft <rwtmedcraft@gmail.com>
2024-10-12 15:47:16 -0400

    add journal button exists, but it looks kinda bad

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 // import App from './App.tsx'
 import Header from './components/header'
 import './index.css'
+import Footer from './components/footer'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Header />
+    <Footer />
   </StrictMode>,
 )
