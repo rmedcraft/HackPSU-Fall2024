ce6ae13d32f3354b622ac7c69ff85f9af9c415d4
Kieran Laverty <44180036+kieranlaverty@users.noreply.github.com>
2024-10-13 08:51:33 -0400

    added function generateJorn

--- server/Mongo.py
+++ server/Mongo.py
@@ -1,18 +1,23 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import pymongo
+import ML_Models
 
 uri = "mongodb+srv://user:password1234@cluster0.amvde.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 
+def generateJorn(text, user):
+    result = ML_Models.classifier(text)
+    createJorn(text, result[1], result[0], user)
+    return
 
 #takes in the text and the score and adds the journal to the database
-def createJorn(text, score, user):
+def createJorn(text, score, label, user):
     db = client[user]
     collection = db["Journals"]
 
-    post = {"text": text, "score": score}
+    post = {"text": text, "score": score, "label": label}
 
     collection.insert_one(post)
 
@@ -31,3 +36,4 @@ def retrieveJorn(user):
         print(x)
     """
     return collection.find({})
+
